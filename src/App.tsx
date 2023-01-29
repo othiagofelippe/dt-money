7341a5df6258995e23d3ed4161dbbedd23c23331
othiagofelippe <thiagofelippe135@gmail.com>
2023-01-29 10:00:50 -0300

    Components: header

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from "styled-components";
+import { Transactions } from "./pages/Transactions";
 import { GlobalStyles } from "./styles/global";
 import { defaultTheme } from "./styles/themes/default";
 
@@ -6,7 +7,7 @@ export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles/>
-      <h1>Hello World</h1>
+      <Transactions />
     </ThemeProvider>
   )
 }
