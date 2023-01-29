99a9a0c64cbe37dfd7448a31773ac37657fdfef4
othiagofelippe <thiagofelippe135@gmail.com>
2023-01-29 09:23:01 -0300

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+export function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles/>
+      <h1>Hello World</h1>
+    </ThemeProvider>
+  )
+}
+
