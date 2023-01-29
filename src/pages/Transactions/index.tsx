444cb1e560743ba840af4e38d96d7201a49917ff
othiagofelippe <thiagofelippe135@gmail.com>
2023-01-29 11:40:57 -0300

    transactions table

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,41 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.000,00</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/04/2002</td>
+            </tr>
+
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHighlight variant="outcome">
+                  - R$ 12.000,00
+                </PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>13/04/2002</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
-  )
+  );
 }
